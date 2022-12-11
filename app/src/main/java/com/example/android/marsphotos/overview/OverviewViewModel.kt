1258eb195e1d234cab4b3a85e15cdd7e4b5f5993
J.D. Holwick <70964794+jdholwick@users.noreply.github.com>
2022-12-11 15:53:12 -0700

    Presently shows one image. Working on getting it to show multiple images.

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -21,8 +21,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.marsphotos.network.MarsApi
-import kotlinx.coroutines.launch
 import com.example.android.marsphotos.network.MarsPhoto
+import kotlinx.coroutines.launch
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -34,6 +34,9 @@ class OverviewViewModel : ViewModel() {
 
     // The external immutable LiveData for the request status
     val status: LiveData<String> = _status
+    private val _photos = MutableLiveData<List<MarsPhoto>>()
+    val photos: LiveData<List<MarsPhoto>> = _photos
+
     /**
      * Call getMarsPhotos() on init so we can display status immediately.
      */
@@ -48,8 +51,11 @@ class OverviewViewModel : ViewModel() {
     private fun getMarsPhotos() {
         viewModelScope.launch {
             try {
-                val listResult = MarsApi.retrofitService.getPhotos()
-                _status.value = "Success: ${listResult.size} Mars photos retrieved"
+                _photos.value = MarsApi.retrofitService.getPhotos()
+                _status.value = "Success: Mars properties retrieved"
+
+                //val listResult = MarsApi.retrofitService.getPhotos()
+                //_status.value = "Success: ${listResult.size} Mars photos retrieved"
             } catch (e: Exception) {
                 _status.value = "Failure: ${e.message}"
             }
